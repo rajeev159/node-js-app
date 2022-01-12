ecb2a099554e24e5e3eb85b5ddfbe7f50946f599
Rajeev <rajeev10.1985@gmail.com>
2022-01-12 18:03:23 +0530

    fixed linring issue

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+
+const app = express();
 
 const utill = require('./utills/utill');
 
 app.get('/', function (req, res) {
-    console.log("Sum of two number is: " + utill.sumtwonum(20, 30))
-    res.send("Hello world! ");
+    console.log('Sum of two number is: ', utill.sumtwonum(20, 30));
+    res.send('Hello world! ');
 });
 
-
 app.listen(3000, function () {
-    console.log('Server is running....')
+    console.log('Server is running....');
 });
